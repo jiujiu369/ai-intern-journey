a4ef1df6758fb875904bcaeeeaa2acf6cf7c2f0b
赵开通 <jiu216361@gmail.com>
2026-07-11 17:46:03 +0800

    chore: ready to push day1

--- day1.md/practice1.py
+++ day1.md/practice1.py
@@ -1,3 +1,21 @@
-print("开始第一天的练习吧")
-name = input("你好，请输入你的名字：")
-print(f"今天我们将进行一些Python练习，{name}！")
+print("练习1：输入年龄、名字和学校名称，并打印一句话")
+name = input("你好，请输入你的名字：").strip()
+while not name:	
+	print("名字不能为空，请重新输入。")
+	name = input("你好，请输入你的名字：").strip()
+	
+
+school = input("请输入你的学校名称：").strip()
+while not school:
+	print("学校名称不能为空，请重新输入。")
+	school = input("请输入你的学校名称：").strip()
+	
+while True:
+	s = input("请输入你的年龄：(请输入整数)").strip()
+
+	age = float(s)
+	if age >= 0:
+		break
+	else:
+			print("输入的年龄必须是正数，请重新输入。")
+print(f"恭喜你，{school}的{name}，你今年{age}岁，完成练习")
